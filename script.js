88a6e0c2240b88414ddf3124f05cad7351f2ab23
Aditi Ashish Gawande <68374073+AditiGawande4@users.noreply.github.com>
2024-11-19 11:48:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const options = {
     method: 'GET',
     headers: {
-        'x-rapidapi-key': RAPIDAPI_KEY_FROM_ENV,
+        'x-rapidapi-key': RAPIDAPI_KEY,
         'x-rapidapi-host': 'weather-api138.p.rapidapi.com'
     }
 };
